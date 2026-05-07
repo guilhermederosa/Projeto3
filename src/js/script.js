4ab372e1944c5b04531580cecc3cfb891a5b0b0d
Guilherme De Rosa <guilhermeperesderosa@gmail.com>
2026-05-07 11:40:54 -0300

    feat:Menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,29 @@
-window.onload=function(){
-    const intro=document.getElementsById("inicio");
+window.onload = function(){
+    const intro=document.getElementById("inicio");
+
     setTimeout(()=>{
-        intro.classList.add("efeito-out");
+        intro.classList.add("efeito-out")
         setTimeout(()=>{
             intro.style.display="none";
-            content.classList.remove("hedden");
-            content.classList.add("efeito-in");
+            content.classList.remove("hidden");
+            content.classList.add("efeito-in")
         },1000)
     },3000)
-    
+
 }
+// Menu hamburguer
+document.addEventListener('DOMContentLoaded',()=>{
+    const menuIcone= document.getElementById("menu-icone");
+    const navMenu = document.querySelector("nav");
+    if(menuIcone && navMenu){
+        menuIcone.onclick=()=>{
+            navMenu.classList.toggle("active");
+            menuIcone.classList.toggle("open");     
+        }
+    }
+})
+
+
 
 function trocar(cor){
     document.body.style.background=cor;
